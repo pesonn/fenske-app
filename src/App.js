5081950de3d715b21f738f748dbf12d39d128f3a
pesonn <43933988+pesonn@users.noreply.github.com>
2020-03-24 23:00:36 +0100

    json angebunden, map funktioniert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./werputztwas";
 
 function App() {
+  useEffect(() => {
+    loadData();
+  }, []);
+  const [zuteil, setZuteil] = useState([]);
+
+  const loadData = () => {
+    setZuteil(data.zuteilung);
+  };
+
+  console.log(zuteil);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World</p>
+        {zuteil.map(zt => (
+          <p key={zt.id}>{zt.name}</p>
+        ))}
       </header>
     </div>
   );
