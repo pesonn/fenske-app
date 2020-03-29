8d0fb6bebe19f7b82ca3d71b49a82dd38cdf30ae
pesonn <43933988+pesonn@users.noreply.github.com>
2020-03-29 03:14:42 +0200

    Zusatztext wird erzeugt

--- src/components/MBView.js
+++ src/components/MBView.js
@@ -8,6 +8,25 @@ function MBView() {
   const { name } = useParams();
   const [mbs, setMBs] = useState([]);
   const [mb, setMB] = useState([]);
+  const [text, setText] = useState({
+    raum: "",
+  });
+
+  function texteZuordnen() {
+    if (mb.room === "Bad 1" || mb.room === "Bad 2") {
+      setText({ raum: "das Bad!" });
+    } else if (mb.room === "Müll") {
+      setText({ raum: "den Müll weg!" });
+    } else if (mb.room === "Küche") {
+      setText({ raum: "die Küche!" });
+    } else if (mb.room === "Wohnen") {
+      setText({ raum: "den Wohnbereich" });
+    } else {
+      setText({
+        raum: "Öhh... hier stimmt was nicht. Sag mal schnell Jan Bescheid!!!",
+      });
+    }
+  }
 
   useEffect(() => {
     firebase
@@ -20,18 +39,20 @@ function MBView() {
           dbdata.push(data);
         });
         setMBs(dbdata);
-        const dbname = dbdata.find(item => item.name === name);
-        if (typeof dbname === undefined) {
-          return;
-        } else {
-          setMB(dbname);
-        }
+        setMB(dbdata.find(item => item.name === name));
+        console.log(mb);
       });
   }, []);
 
+  useEffect(() => {
+    texteZuordnen();
+  }, [mb]); //damit dieser Effect erst läuft, nachdem sich was an den Daten aus MB geändert hat
+
   return (
     <div>
-      <p>{typeof mb === "undefined" ? <Redirect to="/" /> : mb.name}</p>
+      <h1>Moin {typeof mb === "undefined" ? <Redirect to="/" /> : mb.name}</h1>
+      <h2>Du putzt diese Woche...</h2>
+      <h1>{text.raum}</h1>
     </div>
   );
 }
