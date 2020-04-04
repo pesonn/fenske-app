18f34e7172bc744e843b3f3b186476cbe8bc3f2c
pesonn <43933988+pesonn@users.noreply.github.com>
2020-04-04 14:57:04 +0200

    Admin-Seite hinzugefügt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Overview from "./components/Overview";
 import MBView from "./components/MBView";
+import Admin from "./components/Admin";
 import NoMatch from "./components/NoMatch";
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Overview} />
+          <Route path="/Admin" exact component={Admin} />
           <Route path="/:name" component={MBView} />
         </Switch>
       </div>
