bfa760f444a0f7c4b500ef44819cc099689f9b1d
pesonn <43933988+pesonn@users.noreply.github.com>
2020-04-05 17:41:20 +0200

    Update der Datenbank, wenn sobald alle User Status erledigt haben.

--- src/components/MBView.js
+++ src/components/MBView.js
@@ -4,6 +4,7 @@ import "../App.css";
 import "../styles/MBView.css";
 
 import { getFirebaseCollectionFrom } from "../firebase";
+
 // import getFirebaseCollection from "../getFirebase";
 import Emoji from "a11y-react-emoji";
 
@@ -14,6 +15,9 @@ function MBView() {
   const [text, setText] = useState({
     raum: "",
   });
+  const [rooms, setRooms] = useState([]);
+  const [group1, setGroup1] = useState([]);
+  const [group2, setGroup2] = useState([]);
 
   function texteZuordnen() {
     if (mb.room === "Bad 1" || mb.room === "Bad 2") {
@@ -31,7 +35,7 @@ function MBView() {
     }
   }
 
-  useEffect(() => {
+  function getUsersFromDatabase() {
     getFirebaseCollectionFrom("putzplan").onSnapshot((snapshot) => {
       const dbdata = [];
       snapshot.forEach((doc) => {
@@ -40,25 +44,154 @@ function MBView() {
         dbdata.push({ ...data, dbid });
       });
       setMBs(dbdata);
+      // set unique MB for Component View
       setMB(dbdata.find((item) => item.name === name));
     });
+  }
+
+  useEffect(() => {
+    getUsersFromDatabase();
+    getRoomsFromDatabase();
   }, []);
+  useEffect(() => {
+    setGroups(group1, group2);
+    console.log(mbs);
+  }, [mbs]);
 
   useEffect(() => {
     texteZuordnen();
   }, [mb]); //damit dieser Effect erst läuft, nachdem sich was an den Daten aus MB geändert hat
 
   function changeGeputzt() {
-    getFirebaseCollectionFrom("putzplan")
+    /* getFirebaseCollectionFrom("putzplan")
       .doc(mb.dbid)
-      .update({ geputzt: true });
-    console.log(mb.geputzt);
+      .update({ geputzt: true }); */
+    mb.geputzt = true;
+    getFirebaseCollectionFrom("putzplan").doc(mb.dbid).update({
+      geputzt: mb.geputzt,
+    });
+    //TODO: Muss auch erst MBS State aktualisiert werden und dann später das Datenbankupdate erfolgen
   }
   function changeBackGeputzt() {
-    getFirebaseCollectionFrom("putzplan")
+    /* getFirebaseCollectionFrom("putzplan")
       .doc(mb.dbid)
-      .update({ geputzt: false });
-    console.log(mb.geputzt);
+      .update({ geputzt: false }); */
+    mb.geputzt = false;
+    getFirebaseCollectionFrom("putzplan").doc(mb.dbid).update({
+      geputzt: mb.geputzt,
+    });
+    //TODO: Muss auch erst MBS State aktualisiert werden und dann später das Datenbankupdate erfolgen
+  }
+
+  function getRoomsFromDatabase() {
+    getFirebaseCollectionFrom("rooms").onSnapshot((snapshot) => {
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        setRooms(data);
+      });
+    });
+  }
+  function resetDatabase() {
+    mbs.forEach((mb) => {
+      mb.room = "";
+      mb.geputzt = false;
+    });
+  }
+  // MBs werden in Gruppen entsprchend der Badezimmeraufteilung aufgeteilt, Group 1 = Badezimmer 1, Group 2 = Badezimmer 2
+  function setGroups(groupname1, groupname2) {
+    // Find evey MB with Groupname 1
+    for (let i = 0; i < mbs.length; i++) {
+      if (mbs[i].group === "b1") {
+        groupname1.push(mbs[i]);
+      }
+    }
+    // Find every MB with Groupname 2
+    for (let i = 0; i < mbs.length; i++) {
+      if (mbs[i].group === "b2") {
+        groupname2.push(mbs[i]);
+      }
+    }
+  }
+
+  function findOneMbFromGroup(groupname) {
+    // Find only 1 mb
+    let randomNumber = (maxNumber) =>
+      Math.floor(Math.random() * Math.floor(maxNumber));
+    let numberGroup = randomNumber(groupname.length);
+    let mb = groupname[numberGroup];
+    return mb;
+  }
+
+  function setOneBathRoom(id, bathroomname) {
+    let mb = mbs.find((mb) => mb.dbid === id);
+    mb.room = bathroomname;
+  }
+
+  function setBathRooms() {
+    // Datenbank IDs der MBs abrufen
+    let IDfromgroup1 = findOneMbFromGroup(group1).dbid;
+    let IDfromgroup2 = findOneMbFromGroup(group2).dbid;
+    // Badezimmer zuteilen
+    setOneBathRoom(IDfromgroup1, rooms.bathrooms[0]);
+    setOneBathRoom(IDfromgroup2, rooms.bathrooms[1]);
+  }
+
+  function setOtherRooms() {
+    let usedNumbers = [];
+    let forOtherRooms = [];
+    while (usedNumbers.length < rooms.otherrooms.length) {
+      var n = Math.floor(Math.random() * Math.floor(rooms.otherrooms.length));
+      if (usedNumbers.indexOf(n) === -1) usedNumbers.push(n);
+    }
+    // Alle aus MBs, die bisher kein Room zugeteilt bekommen haben werden in forOtherRooms gespeichert
+    mbs.forEach((mb) => {
+      if (mb.room === "") {
+        forOtherRooms.push(mb);
+      }
+    });
+    // Da die Zahlen zufällig im Array gespeichert sind, erfolgt die Zuordnung nach Indexen
+    for (let i = 0; i < forOtherRooms.length; i++) {
+      forOtherRooms[usedNumbers[i]].room = rooms.otherrooms[usedNumbers[i]];
+    }
+  }
+
+  function updateDatabaseGeputzt() {
+    mbs.forEach((mb) => {
+      getFirebaseCollectionFrom("putzplan").doc(mb.dbid).update({
+        geputzt: mb.geputzt,
+      });
+    });
+  }
+
+  function checkForWeeklyUpdate() {
+    changeGeputzt();
+    let everystatus = [];
+    mbs.forEach((mb) => {
+      if (mb.geputzt === true) {
+        everystatus.push(mb.geputzt);
+      }
+    });
+
+    if (everystatus.length === mbs.length) {
+      // room = "", geputzt = "false"
+      resetDatabase();
+      // Badezimmer zuordnen
+      setBathRooms();
+      // restliche Räume zuordnen
+      setOtherRooms();
+      // Daten aus mbs State in Datenbank hochladen
+      mbs.forEach((mb) => {
+        getFirebaseCollectionFrom("putzplan").doc(mb.dbid).update({
+          room: mb.room,
+          geputzt: false,
+        });
+      });
+      alert(
+        "Du hast als letztes geputzt... das ist nichts schlechtes! Immerhin hast du dafür gesorgt, dass die Verteilung für die nächste Woche zufällig neu entschieden wurde! Hab noch einen schönen restlichen Tag!",
+      );
+    }
+    console.log(everystatus);
+    console.log(mbs);
   }
 
   return (
@@ -69,7 +202,7 @@ function MBView() {
       {mb.geputzt === true ? <h1>Für diese Woche bist du durch!</h1> : null}
 
       {mb.geputzt === false ? (
-        <button onClick={changeGeputzt} className="button">
+        <button onClick={checkForWeeklyUpdate} className="button">
           Erledigt!!
         </button>
       ) : (
