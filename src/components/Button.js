935314820b173b97b91b5fd2a8e8333d5518ca81
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-20 13:15:48 +0200

    an neue Themestruktur angepasst

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Button(props) {
+  const thememode = "light";
+  const apptheme = "putzt";
   const Button = styled.button`
     width: 23vh;
     height: 5vh;
     border: 0;
-    background-color: ${(props) => props.theme.putzt.colors.button};
-    color: ${(props) => props.theme.main.colors.white};
-    font-family: ${(props) => props.theme.main.fontFamily.subline};
-    font-size: ${(props) => props.theme.main.fontSizes.subline};
-    box-shadow: 0px 4px 6px ${(props) => props.theme.putzt.colors.buttonShadow};
+    background-color: ${(props) =>
+      props.theme[thememode][apptheme].colors.button};
+    color: ${(props) => props.theme[thememode].maincolors.white};
+    font-family: ${(props) => props.theme.general.fontFamily.subline};
+    font-size: ${(props) => props.theme.general.fontSizes.subline};
+    box-shadow: 0px 4px 6px
+      ${(props) => props.theme[thememode][apptheme].colors.buttonShadow};
   `;
 
   return <Button onClick={props.callFunction}>{props.text}</Button>;
