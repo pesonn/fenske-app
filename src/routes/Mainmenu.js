c719369247d58a3107e619b409b3890132e26dcb
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-22 13:50:12 +0200

    styles für mainmenu

--- src/routes/Mainmenu.js
+++ src/routes/Mainmenu.js
@@ -1,23 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 import Button from "../components/Button";
+import AppTitle from "../components/AppTitle";
 
 export default function Mainmenu(props) {
   return (
-    <>
+    <Menuwrapper>
+      <StyledAppTitle
+        thememode={props.thememode}
+        apptheme="mainmenu"
+        appdetails={{
+          name: "Die Fenske.app",
+          description: "Was möchtest du machen?",
+        }}
+      ></StyledAppTitle>
       <a href="/putzt">
-        <Button
-          text="Putzt"
-          apptheme={"putzt"}
-          thememode={props.thememode}
-        ></Button>
+        <MenuButton apptheme={"putzt"} thememode={props.thememode}>
+          Putzt
+        </MenuButton>
       </a>
       <a href="/glotzt">
-        <Button
-          text="Glotzt"
-          apptheme={"glotzt"}
-          thememode={props.thememode}
-        ></Button>
+        <MenuButton apptheme={"glotzt"} thememode={props.thememode}>
+          Glotzt
+        </MenuButton>
       </a>
-    </>
+    </Menuwrapper>
   );
 }
+
+const StyledAppTitle = styled(AppTitle)`
+  margin-bottom: 5vh;
+`;
+
+const Menuwrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: space-around;
+  height: 65vh;
+  width: 80vw;
+  margin-left: 10vw;
+  padding: 5vh 0 0 0;
+  min-height: 350px;
+`;
+
+const MenuButton = styled(Button)`
+  width: 45vh;
+  height: 10vh;
+  font-size: 2.8vh;
+`;
