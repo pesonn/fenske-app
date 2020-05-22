cf7c84947542875b737c4b27dbe232a5764fdd53
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-22 13:40:19 +0200

    App Titel eingefügt. Button positioniert

--- src/routes/glotzt/GlotztMenu.js
+++ src/routes/glotzt/GlotztMenu.js
@@ -1,30 +1,57 @@
 import React from "react";
+import styled from "styled-components";
 import Button from "../../components/Button";
+import AppTitle from "../../components/AppTitle";
 
 export default function GlotztMenu(props) {
   return (
-    <>
-      <a href="/rausvoten">
-        <Button
-          thememode={props.thememode}
-          apptheme={props.apptheme}
-          text="Rausvoten"
-        ></Button>
+    <Menuwrapper>
+      <StyledAppTitle
+        thememode={props.thememode}
+        apptheme={props.apptheme}
+        className={props.className}
+        appdetails={{
+          name: "Fenske glotzt!",
+          description: "Wähle einen Modus aus!",
+        }}
+      ></StyledAppTitle>
+      <a href="/glotzt/rausvoten">
+        <MenuButton thememode={props.thememode} apptheme={props.apptheme}>
+          Rausvoten
+        </MenuButton>
       </a>
-      <a href="/bepunktet">
-        <Button
-          thememode={props.thememode}
-          apptheme={props.apptheme}
-          text="Bepunktet"
-        ></Button>
+      <a href="/glotzt/bepunktet">
+        <MenuButton thememode={props.thememode} apptheme={props.apptheme}>
+          Bepunktet
+        </MenuButton>
       </a>
-      <a href="/top100">
-        <Button
-          thememode={props.thememode}
-          apptheme={props.apptheme}
-          text="Top 100 Poster"
-        ></Button>
+      <a href="/glotzt/top100">
+        <MenuButton thememode={props.thememode} apptheme={props.apptheme}>
+          Top 100 Poster
+        </MenuButton>
       </a>
-    </>
+    </Menuwrapper>
   );
 }
+
+const StyledAppTitle = styled(AppTitle)`
+  margin-bottom: 5vh;
+`;
+
+const Menuwrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: space-around;
+  height: 65vh;
+  width: 80vw;
+  margin-left: 10vw;
+  padding: 5vh 0 0 0;
+  min-height: 350px;
+`;
+
+const MenuButton = styled(Button)`
+  width: 45vh;
+  height: 10vh;
+  font-size: 2.8vh;
+`;
