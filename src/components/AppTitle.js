d281afa4ab87ea76c69ad6b55ffcee04f1f35930
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-22 13:13:10 +0200

    styled components aus render method ausgelagert

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -2,35 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 export default function AppTitle(props) {
-  const thememode = props.thememode;
-  const apptheme = props.apptheme;
-
-  const AppWrapper = styled.div`
-    width: 100%;
-    text-align: left;
-    margin-bottom: 8vh;
-  `;
-
-  const AppName = styled.h1`
-    font-family: ${(props) => props.theme.general.fontFamily.headline};
-    font-size: ${(props) => props.theme.general.fontSizes.headline};
-
-    color: ${(props) => props.theme[thememode][apptheme].colors.headline};
-    width: 100%;
-    margin-left: -3px;
-  `;
-  const AppDescription = styled.h2`
-    font-family: ${(props) => props.theme.general.fontFamily.subline};
-    font-size: ${(props) => props.theme.general.fontSizes.subline};
-    color: ${(props) => props.theme[thememode].maincolors.text};
-    margin-top: 10px;
-    width: 100%;
-  `;
-
   return (
     <AppWrapper>
-      <AppName>{props.appdetails.name}</AppName>
-      <AppDescription>{props.appdetails.description}</AppDescription>
+      <AppName thememode={props.thememode} apptheme={props.apptheme}>
+        {props.appdetails.name}
+      </AppName>
+      <AppDescription thememode={props.thememode} apptheme={props.apptheme}>
+        {props.appdetails.description}
+      </AppDescription>
     </AppWrapper>
   );
 }
+const AppWrapper = styled.div`
+  width: 100%;
+  text-align: left;
+  margin-bottom: 8vh;
+`;
+
+const AppName = styled.h1`
+  font-family: ${(props) => props.theme.general.fontFamily.headline};
+  font-size: ${(props) => props.theme.general.fontSizes.headline};
+
+  color: ${(props) =>
+    props.theme[props.thememode][props.apptheme].colors.headline};
+  width: 100%;
+  margin-left: -3px;
+`;
+const AppDescription = styled.h2`
+  font-family: ${(props) => props.theme.general.fontFamily.subline};
+  font-size: ${(props) => props.theme.general.fontSizes.subline};
+  color: ${(props) => props.theme[props.thememode].maincolors.text};
+  margin-top: 10px;
+  width: 100%;
+`;
