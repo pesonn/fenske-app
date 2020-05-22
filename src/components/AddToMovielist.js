7bbfd1d6f98a98318433e2bac9525205fa8b1d87
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-22 15:39:16 +0200

    Struktur um Film hinzuzufügen

---
+++ src/components/AddToMovielist.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddToMovielist() {
+  /*
+  1. Eingabefeld -> eigene component
+  2. eingegeben Namen per props an Providerauswahl, von da in Datenbank schieben und zurück zur Movielist Übersicht
+  */
+  return <div></div>;
+}
---
+++ src/components/Inputfied.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Inputfied() {
+  return <div></div>;
+}
