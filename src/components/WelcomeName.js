bcd4ce85c75cc216f905f0a76d605f213d8017c0
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-22 13:09:36 +0200

    styled components aus render method ausgelagert

--- src/components/WelcomeName.js
+++ src/components/WelcomeName.js
@@ -3,36 +3,13 @@ import { Redirect } from "react-router-dom";
 import styled from "styled-components";
 
 export default function WelcomeName(props) {
-  const thememode = props.thememode;
-  const apptheme = props.apptheme;
-
-  const Wrapper = styled.div`
-    display: flex;
-    flex-flow: column wrap;
-    justify-content: center;
-    align-items: center;
-    margin-top: 10vh;
-  `;
-  const WelcomeMB = styled.h1`
-    font-family: ${(props) => props.theme.general.fontFamily.headline};
-    font-size: ${(props) => props.theme.general.fontSizes.headline};
-    color: ${(props) => props.theme[thememode][apptheme].colors.headline};
-  `;
-
-  const Description = styled.h6`
-    font-family: ${(props) => props.theme.general.fontFamily.subline};
-    font-size: ${(props) => props.theme.general.fontSizes.subline};
-    color: ${(props) => props.theme[thememode].maincolors.text};
-    margin-bottom: 6vh;
-  `;
-
   return (
     <Wrapper>
-      <WelcomeMB>
+      <WelcomeMB thememode={props.thememode} apptheme={props.apptheme}>
         Moin{" "}
         {typeof props.mb === "undefined" ? <Redirect to="/" /> : props.mb.name}!
       </WelcomeMB>
-      <Description>
+      <Description thememode={props.thememode} apptheme={props.apptheme}>
         {props.mb.geputzt
           ? "Für diese Woche bist du durch!"
           : "Hast du für diese Woche alles erledigt?"}
@@ -40,3 +17,23 @@ export default function WelcomeName(props) {
     </Wrapper>
   );
 }
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10vh;
+`;
+const WelcomeMB = styled.h1`
+  font-family: ${(props) => props.theme.general.fontFamily.headline};
+  font-size: ${(props) => props.theme.general.fontSizes.headline};
+  color: ${(props) =>
+    props.theme[props.thememode][props.apptheme].colors.headline};
+`;
+
+const Description = styled.h6`
+  font-family: ${(props) => props.theme.general.fontFamily.subline};
+  font-size: ${(props) => props.theme.general.fontSizes.subline};
+  color: ${(props) => props.theme[props.thememode].maincolors.text};
+  margin-bottom: 6vh;
+`;
