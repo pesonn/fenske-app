caf4b17cdd7122d6ab6387c1de5ad379843b2fd1
pesonn <43933988+pesonn@users.noreply.github.com>
2020-05-22 12:49:55 +0200

    Styled component aus render Method ausgelagert

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Button(props) {
-  const thememode = props.thememode;
-  const apptheme = props.apptheme;
-  const Button = styled.button`
-    width: 23vh;
-    height: 5vh;
-    border: 0;
-    background-color: ${(props) =>
-      props.theme[thememode][apptheme].colors.button};
-    color: ${(props) => props.theme[thememode].maincolors.white};
-    font-family: ${(props) => props.theme.general.fontFamily.subline};
-    font-size: ${(props) => props.theme.general.fontSizes.subline};
-    box-shadow: 0px 4px 6px
-      ${(props) => props.theme[thememode][apptheme].colors.buttonShadow};
-  `;
-
-  return <Button onClick={props.callFunction}>{props.text}</Button>;
+const Button = styled.button`
+  width: 23vh;
+  height: 5vh;
+  background-color: ${(props) =>
+    props.theme[props.thememode][props.apptheme].colors.button};
+  border: 0;
+  background-color: ${(props) =>
+    props.theme[props.thememode][props.apptheme].colors.button};
+  color: ${(props) => props.theme[props.thememode].maincolors.white};
+  font-family: ${(props) => props.theme.general.fontFamily.subline};
+  font-size: ${(props) => props.theme.general.fontSizes.subline};
+  box-shadow: 0px 4px 6px
+    ${(props) =>
+      props.theme[props.thememode][props.apptheme].colors.buttonShadow};
+`;
+export default function StyledButton(props) {
+  return (
+    <Button
+      className={props.className}
+      thememode={props.thememode}
+      apptheme={props.apptheme}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </Button>
+  );
 }
