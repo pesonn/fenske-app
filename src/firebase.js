59c42079007bae192413e10bdf21de5f015c86c0
pesonn <43933988+pesonn@users.noreply.github.com>
2020-06-24 21:37:33 +0200

    Anmeldung mittels firebaseui and react-firebaseui funktioniert

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import firebase from "firebase/app";
+import "firebase/auth";
+import * as firebaseui from "firebaseui";
 import "firebase/firestore";
 
 // Your web app's Firebase configuration
--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -1,69 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { getFirebaseCollectionFrom } from "../firebase";
+import firebase, { getFirebaseCollectionFrom } from "../firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import AppTitle from "../components/AppTitle";
 import Button from "../components/Button";
 
 export default function Admin(props) {
-  const [data, setData] = useState({});
-
-  const getDatafromDatabase = () => {
-    getFirebaseCollectionFrom("test").onSnapshot((snapshot) => {
-      const dbdata = [];
-      snapshot.forEach((doc) => {
-        const data = doc.data();
-        const dbid = doc.id;
-        dbdata.push({ ...data, dbid });
-        setData(dbdata);
-      });
-    });
-  };
-
-  useEffect(() => {
-    getDatafromDatabase();
-  }, []);
-  const leer = () => {};
-
-  let dibidada = { dbid: "" };
-  const updateDataInDatabase = () => {
-    getFirebaseCollectionFrom("rausvoten").doc("Findet Nemo").add({
-      name: "Findet Nemo",
-      active: true,
-      provider: "Disney+",
-    });
-
-    getFirebaseCollectionFrom("rausvoten")
-      .doc("HfRKCameXsXlLZzWhbiF")
-      .collection("movielist")
-      .doc("Harry Potter")
-      .update({
-        active: false,
-      });
-    console.log(data);
-
-    getFirebaseCollectionFrom("rausvoten")
-      .doc("HfRKCameXsXlLZzWhbiF")
-      .collection("movielist")
-      .doc(dibidada.dbid)
-      .update({
-        active: false,
-      });
+  const uiConfig = {
+    signInFlow: "popup",
+    signInSuccessUrl: "/",
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: true,
+      },
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
   };
 
   return (
     <>
       <AppTitle
         appdetails={{ name: "Hello Admin", description: "" }}
-        thememode={props.thememode}
-        apptheme={props.apptheme}
       ></AppTitle>
-      <Button
-        onClick={updateDataInDatabase}
-        thememode={props.thememode}
-        apptheme={props.apptheme}
-      >
-        starte den Test
-      </Button>
-      {console.log(data)}
+      <Button>starte den Test</Button>
+
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </>
   );
 }
