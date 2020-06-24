4a6a13405c5080e9c8f9380f8bb95f609d3ce6e3
pesonn <43933988+pesonn@users.noreply.github.com>
2020-06-24 23:24:21 +0200

    Willkommenseite wird angezeigt, wenn Nutzer nicht eingelogt ist.

---
+++ src/routes/Welcome.js
@@ -0,0 +1,33 @@
+import React from "react";
+import firebase from "firebase/app";
+import styled from "styled-components";
+import AppTitle from "../components/AppTitle";
+import FirebaseAuthFields from "../components/FirebaseAuthFields";
+
+export default function Welcome() {
+  let user = firebase.auth().currentUser;
+  console.log(user);
+  return (
+    <MenuWrapper>
+      <AppTitle
+        appdetails={{
+          name: "Moin!",
+          description: "Melde dich an, um die Fenske App zu benutzen!",
+        }}
+      />
+      <FirebaseAuthFields />
+    </MenuWrapper>
+  );
+}
+
+const MenuWrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: center;
+  height: 65vh;
+  width: 80vw;
+  margin-left: 10vw;
+  padding: 0vh 0 0 0;
+  min-height: 350px;
+`;
