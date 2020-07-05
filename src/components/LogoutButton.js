fb20696f8de4eca7488ec2b48bee531f3dc46d0c
pesonn <43933988+pesonn@users.noreply.github.com>
2020-07-05 19:06:40 +0200

    new component logout Button

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
-import { ThemeMode, AppTheme } from "../App";
+import { ThemeMode, AppTheme, UserData } from "../App";
 import Button from "./Button";
-import firebase from "../firebase"
+import firebase from "../firebase";
 
 export default function LogoutButton(props) {
   const thememode = useContext(ThemeMode);
+  const user = useContext(UserData);
   return (
-  <> 
-    { user ? 
-    (<StyledLogoutButton
-      className={props.className}
-      thememode={thememode}
-      onClick={props.onClick}
-    />)
-    :
-    (<></>)}
-  </>
+    <>
+      {user ? (
+        <StyledLogoutButton
+          className={props.className}
+          thememode={thememode}
+          onClick={() => firebase.auth().signOut()}
+        >
+          {props.children}
+        </StyledLogoutButton>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 const StyledLogoutButton = styled(Button)`
   background-color: ${(props) => props.theme[props.thememode].maincolors.black};
+  font-size: ${(props) => props.theme.general.fontSizes.smallButton};
+  width: 10vh;
+  height: 3vh;
 `;
