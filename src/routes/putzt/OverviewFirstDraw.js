eb7174d49216af30693c39f4150ee30bae98558d
pesonn <43933988+pesonn@users.noreply.github.com>
2020-07-22 01:05:08 +0200

    Funktion zur ersten Auslosung des Putzplanes integriert

--- src/routes/putzt/OverviewFirstDraw.js
+++ src/routes/putzt/OverviewFirstDraw.js
@@ -7,7 +7,6 @@ import OverviewName from "../../components/OverviewName";
 import AppTitle from "../../components/AppTitle";
 import Button from "../../components/Button";
 import { UserData } from "../../App";
-import CompleteTask from "../../components/CompleteTask";
 import { Putzplanung, PutzplanGroupData } from "./Overview";
 import { WeekNumber } from "../../components/WeekNumber";
 
@@ -35,96 +34,6 @@ export default function OverviewFirstDraw(props) {
     getUsersFromDatabase();
   }, []);
 
-  /*   let newMBs = mbs;
-const setNewRooms = () => {
-    const resetMbs = () => {
-      return mbs.map((item) => ({
-        ...item,
-        room: "",
-        geputzt: false,
-      }));
-    };
-    newMBs = resetMbs();
-
-    // Badezimmer zuordnen
-    const setBathRooms = () => {
-      let usedNumbers = [];
-      while (usedNumbers.length < putzplandata.rooms.bathrooms.length) {
-        var n = Math.floor(
-          Math.random() * Math.floor(putzplandata.rooms.bathrooms.length),
-        );
-        if (usedNumbers.indexOf(n) === -1) {
-          usedNumbers.push(n);
-        }
-      }
-      const setOneBathRoom = (group, bathroomname) => {
-        let bathgroup = newMBs.filter((item) => item.group === group);
-        let randomindex = Math.floor(
-          Math.random() * Math.floor(bathgroup.length),
-        );
-        newMBs.find(
-          (item) => bathgroup[randomindex].name === item.name,
-        ).room = bathroomname;
-      };
-
-      setOneBathRoom("b1", putzplandata.rooms.bathrooms[0]);
-      setOneBathRoom("b2", putzplandata.rooms.bathrooms[1]);
-    };
-    setBathRooms();
-
-    // restliche Räume zuordnen
-    const setOtherRooms = () => {
-      let forOtherRooms = [];
-      let usedNumbers = [];
-      while (usedNumbers.length < putzplandata.rooms.otherrooms.length) {
-        var n = Math.floor(
-          Math.random() * Math.floor(putzplandata.rooms.otherrooms.length),
-        );
-        if (usedNumbers.indexOf(n) === -1) {
-          usedNumbers.push(n);
-        }
-      }
-      // Alle aus MBs, die bisher kein Room zugeteilt bekommen haben werden in forOtherRooms gespeichert
-      newMBs.forEach((mb) => (mb.room === "" ? forOtherRooms.push(mb) : null));
-      //usedNumbers.splice(-usedNumbers.length, forOtherRooms.length);
-      console.log(usedNumbers);
-      // Da die Zahlen zufällig im Array gespeichert sind, erfolgt die Zuordnung nach Indexen
-      for (let i = 0; i < forOtherRooms.length; i++) {
-        //TODO: Hier wirft er einen Fehler aus "cannot set property room of undefined"
-        console.log(usedNumbers);
-        forOtherRooms[usedNumbers[i]].room = putzplandata.rooms.otherrooms[i];
-        console.log(forOtherRooms[0].room);
-      }
-    };
-    setOtherRooms();
-
-    // Daten aus newMBs in Datenbank hochladen
-    newMBs.forEach((item) => {
-      getFirebaseCollectionFrom("putzt-app")
-        .doc(user.putztID)
-        .collection("putzplan")
-        .doc(item.dbid)
-        .update({
-          room: item.room,
-          geputzt: false,
-        });
-    });
-
-    // Fals die Auslosung der neuen Aufgaben in der Folgewoche erfolgt, darf nicht die nächste Woche gesetzt werden, sondern es muss die aktuelle Woche gesetzt werden.
-    let correctedweeknumber;
-    if (WeekNumber().nextweek - putzplandata.weeknumber < 2) {
-      correctedweeknumber = WeekNumber().nextweek;
-    } else {
-      correctedweeknumber = WeekNumber().thisweek;
-    }
-
-    getFirebaseCollectionFrom("putzt-app").doc(user.putztID).update({
-      lastupdate: new Date(),
-      weeknumber: correctedweeknumber,
-    });
-  };
-
-  // Um eine spezielle Ansicht zum ersten Auslosen des Putzplanes zu erstellen */
   let firstDraw = () => {
     let newmbs = mbs;
     let everyroom = [];
@@ -174,9 +83,9 @@ const setNewRooms = () => {
   return (
     <>
       <OverviewList>
-        <AppTitle
+        <StyledAppTitle
           appdetails={{
-            name: "Fenske putzt!",
+            name: `Fenske putzt! - Invitecode: ${putzplandata.invitecode}`,
             description:
               "Sobald alle beigetreten sind könnt ihr den Putzplan starten",
           }}
@@ -194,26 +103,25 @@ const setNewRooms = () => {
   );
 }
 
+const StyledAppTitle = styled(AppTitle)`
+  margin-bottom: 0vh;
+`;
+
 const OverviewList = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: top;
   width: 80vw;
+  height: 80vh;
   margin-left: 10vw;
   padding: 8vh 0 0 0;
   min-height: 350px;
 `;
 
 const ListOfNames = styled.section`
-  height: 50vh;
+  height: 60%;
   width: 100%;
   margin-top: 3vh;
   display: flex;
   flex-wrap: wrap;
-`;
-
-const LegalsLink = styled.sub`
-  position: absolute;
-  bottom: 15px;
-  right: 30px;
-  font-size: 0.8rem;
 `;
