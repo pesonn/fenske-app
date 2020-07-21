2c54c7365c56e6c95357d36b7b72719b58a16b03
pesonn <43933988+pesonn@users.noreply.github.com>
2020-07-22 00:47:07 +0200

    erste Auslosung der Gruppe funktioniert

--- src/routes/putzt/OverviewFirstDraw.js
+++ src/routes/putzt/OverviewFirstDraw.js
@@ -1,45 +1,176 @@
 import React, { useState, useEffect, useContext, createContext } from "react";
+import { useParams } from "react-router-dom";
 import firebase, { getFirebaseCollectionFrom } from "../../firebase";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import OverviewName from "../../components/OverviewName";
 import AppTitle from "../../components/AppTitle";
+import Button from "../../components/Button";
 import { UserData } from "../../App";
-
-export const PutzplanData = createContext();
-export const Putzplanung = createContext();
+import CompleteTask from "../../components/CompleteTask";
+import { Putzplanung, PutzplanGroupData } from "./Overview";
+import { WeekNumber } from "../../components/WeekNumber";
 
 export default function OverviewFirstDraw(props) {
-  const [putzplanung, setPutzplanung] = useState([]);
   const user = useContext(UserData);
+  const putzplanung = useContext(Putzplanung);
+  const putzplandata = useContext(PutzplanGroupData);
+  const [mbs, setMBs] = useState([]);
 
-  // const get
-  const getPutzplanung = () => {
-    if (user) {
+  function getUsersFromDatabase() {
+    getFirebaseCollectionFrom("putzt-app")
+      .doc(user.putztID)
+      .collection("putzplan")
+      .onSnapshot((snapshot) => {
+        const dbdata = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          const dbid = doc.id;
+          dbdata.push({ ...data, dbid, room: "" });
+        });
+        setMBs(dbdata);
+      });
+  }
+  useEffect(() => {
+    getUsersFromDatabase();
+  }, []);
+
+  /*   let newMBs = mbs;
+const setNewRooms = () => {
+    const resetMbs = () => {
+      return mbs.map((item) => ({
+        ...item,
+        room: "",
+        geputzt: false,
+      }));
+    };
+    newMBs = resetMbs();
+
+    // Badezimmer zuordnen
+    const setBathRooms = () => {
+      let usedNumbers = [];
+      while (usedNumbers.length < putzplandata.rooms.bathrooms.length) {
+        var n = Math.floor(
+          Math.random() * Math.floor(putzplandata.rooms.bathrooms.length),
+        );
+        if (usedNumbers.indexOf(n) === -1) {
+          usedNumbers.push(n);
+        }
+      }
+      const setOneBathRoom = (group, bathroomname) => {
+        let bathgroup = newMBs.filter((item) => item.group === group);
+        let randomindex = Math.floor(
+          Math.random() * Math.floor(bathgroup.length),
+        );
+        newMBs.find(
+          (item) => bathgroup[randomindex].name === item.name,
+        ).room = bathroomname;
+      };
+
+      setOneBathRoom("b1", putzplandata.rooms.bathrooms[0]);
+      setOneBathRoom("b2", putzplandata.rooms.bathrooms[1]);
+    };
+    setBathRooms();
+
+    // restliche Räume zuordnen
+    const setOtherRooms = () => {
+      let forOtherRooms = [];
+      let usedNumbers = [];
+      while (usedNumbers.length < putzplandata.rooms.otherrooms.length) {
+        var n = Math.floor(
+          Math.random() * Math.floor(putzplandata.rooms.otherrooms.length),
+        );
+        if (usedNumbers.indexOf(n) === -1) {
+          usedNumbers.push(n);
+        }
+      }
+      // Alle aus MBs, die bisher kein Room zugeteilt bekommen haben werden in forOtherRooms gespeichert
+      newMBs.forEach((mb) => (mb.room === "" ? forOtherRooms.push(mb) : null));
+      //usedNumbers.splice(-usedNumbers.length, forOtherRooms.length);
+      console.log(usedNumbers);
+      // Da die Zahlen zufällig im Array gespeichert sind, erfolgt die Zuordnung nach Indexen
+      for (let i = 0; i < forOtherRooms.length; i++) {
+        //TODO: Hier wirft er einen Fehler aus "cannot set property room of undefined"
+        console.log(usedNumbers);
+        forOtherRooms[usedNumbers[i]].room = putzplandata.rooms.otherrooms[i];
+        console.log(forOtherRooms[0].room);
+      }
+    };
+    setOtherRooms();
+
+    // Daten aus newMBs in Datenbank hochladen
+    newMBs.forEach((item) => {
       getFirebaseCollectionFrom("putzt-app")
         .doc(user.putztID)
         .collection("putzplan")
-        .orderBy("name", "asc")
-        .onSnapshot((snapshot) => {
-          let putzplan = [];
-          snapshot.forEach((doc) => {
-            putzplan.push(doc.data());
-          });
-          setPutzplanung(putzplan);
+        .doc(item.dbid)
+        .update({
+          room: item.room,
+          geputzt: false,
         });
+    });
+
+    // Fals die Auslosung der neuen Aufgaben in der Folgewoche erfolgt, darf nicht die nächste Woche gesetzt werden, sondern es muss die aktuelle Woche gesetzt werden.
+    let correctedweeknumber;
+    if (WeekNumber().nextweek - putzplandata.weeknumber < 2) {
+      correctedweeknumber = WeekNumber().nextweek;
+    } else {
+      correctedweeknumber = WeekNumber().thisweek;
     }
+
+    getFirebaseCollectionFrom("putzt-app").doc(user.putztID).update({
+      lastupdate: new Date(),
+      weeknumber: correctedweeknumber,
+    });
   };
 
-  useEffect(() => {
-    getPutzplanung();
-  }, [user]);
+  // Um eine spezielle Ansicht zum ersten Auslosen des Putzplanes zu erstellen */
+  let firstDraw = () => {
+    let newmbs = mbs;
+    let everyroom = [];
+    everyroom.push(
+      ...putzplandata.rooms.bathrooms,
+      ...putzplandata.rooms.otherrooms,
+    );
 
-  // Um eine spezielle Ansicht zum ersten Auslosen des Putzplanes zu erstellen
-  let firstDraw = getFirebaseCollectionFrom("putzt-app")
-    .doc(user.putztID)
-    .onSnapshot((snapshot) => {
-      return snapshot.data().firstDraw;
+    let usedNumbers = [];
+    //TODO: Hier provisorisch auf die maximale Anzahl an Teilnehmern begrenzt
+    while (usedNumbers.length < mbs.length) {
+      var n = Math.floor(Math.random() * Math.floor(everyroom.length));
+      if (usedNumbers.indexOf(n) === -1) {
+        usedNumbers.push(n);
+      }
+    }
+    console.log(usedNumbers);
+
+    for (let i = 0; i < mbs.length; i++) {
+      //TODO: Hier wirft er einen Fehler aus "cannot set property room of undefined"
+      newmbs[i].room = everyroom[usedNumbers[i]];
+    }
+    console.log(newmbs);
+
+    newmbs.forEach((item) => {
+      getFirebaseCollectionFrom("putzt-app")
+        .doc(user.putztID)
+        .collection("putzplan")
+        .doc(item.dbid)
+        .update({
+          room: item.room,
+          geputzt: false,
+        });
     });
+
+    getFirebaseCollectionFrom("putzt-app").doc(user.putztID).update({
+      lastupdate: new Date(),
+      weeknumber: WeekNumber().thisweek,
+      firstDrawDone: true,
+    });
+  };
+
+  // getFirebaseCollectionFrom("putzt-app")
+  //   .doc(user.putztID)
+  //   .onSnapshot((snapshot) => {
+  //     return snapshot.data().firstDraw;})
   return (
     <>
       <OverviewList>
@@ -50,16 +181,14 @@ export default function OverviewFirstDraw(props) {
               "Sobald alle beigetreten sind könnt ihr den Putzplan starten",
           }}
         />
-        <Putzplanung.Provider value={putzplanung}>
-          <ListOfNames>
-            {putzplanung.map((item) => (
-              <>
-                <OverviewName item={item} showindicator={false} />
-              </>
-            ))}
-          </ListOfNames>
-        </Putzplanung.Provider>
-        <Button></Button>
+        <ListOfNames>
+          {putzplanung.map((item) => (
+            <>
+              <OverviewName item={item} showindicator={false} />
+            </>
+          ))}
+        </ListOfNames>
+        <Button onClick={firstDraw}>Wir sind vollzählig!</Button>
       </OverviewList>
     </>
   );
